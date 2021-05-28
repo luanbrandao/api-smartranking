0a88132bb7e58589656758dca14aaf42c85c315f
arcanjo <luanbrandao4@gmail.com>
2021-05-28 00:46:38 -0300

    feat: create JogadoresService.criarAtualizarJogador

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+  constructor(private readonly jogadoresServices: JogadoresService) {}
+
   @Post()
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     const { email } = criarJogadorDto;
 
-    return JSON.stringify(`{
-     "nome": ${email},
-    }`);
+    await this.jogadoresServices.criarAtualizarJogador(criarJogadorDto);
   }
 }
