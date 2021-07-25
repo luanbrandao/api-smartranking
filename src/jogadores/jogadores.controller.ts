0d24e377f0fcbbfc7c45e77e03391da055400248
arcanjo <luanbrandao4@gmail.com>
2021-07-25 14:21:22 -0300

    feat: 23. ValidationPipe para validação dos dados

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interface/jogador.interface';
 import { JogadoresService } from './jogadores.service';
@@ -8,6 +17,7 @@ export class JogadoresController {
   constructor(private readonly jogadoresServices: JogadoresService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     const { email } = criarJogadorDto;
 
