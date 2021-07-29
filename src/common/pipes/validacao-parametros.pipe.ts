829fb779538896682505387666b5d1ae6b99cdcf
arcanjo <luanbrandao4@gmail.com>
2021-07-28 22:58:45 -0300

    feat: 30. Entidade Categorias [ Parte 4 ]

--- src/common/pipes/validacao-parametros.pipe.ts
+++ src/common/pipes/validacao-parametros.pipe.ts
@@ -1,14 +1,11 @@
 import {
+  PipeTransform,
   ArgumentMetadata,
   BadRequestException,
-  PipeTransform,
 } from '@nestjs/common';
 
-export class JogadoresValidacaoParametrosPipe implements PipeTransform {
+export class ValidacaoParametrosPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    console.log('values', value);
-    console.log('metadata', metadata);
-
     if (!value) {
       throw new BadRequestException(
         `O valor do parametro ${metadata.data} deve ser informado`,
